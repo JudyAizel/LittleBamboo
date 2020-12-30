1fdd7fc87777ae0844d2aa565bbc6f917ab613c4
Judy Aizel Utayde <jajasemanautayde@gmail.com>
2020-12-30 21:59:32 +0800

    try

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
 
 <head>
-    <title>LittleBamboo - Judy's Delicacies</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="css/index.css" />
+    <title>LittleBamboo - Judy's Delicacies</title>
+  
 </head>
 
 <body>
